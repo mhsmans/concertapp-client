95c9669b671b863dc63db0c6ee99d353c99d2e03
mhsmans <mh.smans@gmail.com>
2019-03-26 16:50:25 +0100

    Style set-up

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,71 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>|
-      <router-link to="/about">About</router-link>
+  <div class="main-container">
+    <div id="app">
+      <div id="nav">
+        <div class="left-group">
+          <router-link :to="{name: 'home'}">
+            <p class="branding">Concert-app</p>
+          </router-link>
+        </div>
+        <div class="right-group">
+          <router-link to="/">Home</router-link>
+          <router-link to="/about">About</router-link>
+        </div>
+      </div>
+      <div class="container">
+        <transition name="fade" mode="out-in">
+          <router-view/>
+        </transition>
+      </div>
     </div>
-    <router-view/>
   </div>
 </template>
 
 <style lang="scss">
+@import url("https://fonts.googleapis.com/css?family=Exo+2");
+
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: "Exo 2", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: $color-white;
+}
+
+#nav {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  padding: 20px 80px;
+  background-color: $color-background-header;
+  @include nav-shadow;
+
+  a {
+    font-size: 1.4em;
+    font-weight: bold;
+    color: $color-white;
+  }
+
+  > .right-group > a {
+    &.router-link-exact-active {
+      color: $color-primary;
+    }
+  }
+}
+
+.branding {
+  margin: 0;
+  width: fit-content;
+}
+
+.right-group {
+  text-align: right;
+
+  a {
+    margin: 0px 10px;
+    transition: color 0.3s ease;
+  }
+  :last-child {
+    margin-right: 0;
+  }
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <div class="featured-title">
+      <h1>Featured</h1>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -8,3 +12,12 @@ import { Component, Vue } from "vue-property-decorator";
 @Component({})
 export default class Home extends Vue {}
 </script>
+
+<style lang="scss" scoped>
+.featured-title {
+  width: fit-content;
+  padding: 0 10px;
+  border-left: 4px solid $color-primary;
+}
+</style>
+
